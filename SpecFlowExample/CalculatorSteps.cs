c5d5dd4bef8af20b422d1dcec72264f1689f0a81
Mike Gardner <mjgardner@abitofhelp.com>
2018-04-27 13:49:59 -0700

    Initial version

---
+++ SpecFlowExample/CalculatorSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using MyCalculator;
+using Xunit;
+
+namespace BddSample
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private readonly Calculator _calculator = new Calculator();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            var result = _calculator.Add();
+
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.Equal(expectedResult, _calculator.Add());
+        }
+    }
+}
